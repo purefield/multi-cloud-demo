5978927292370e3b3604106a1511a38b4533602a
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-07 14:28:13 -0500

    Adding hostname to logs

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 const VERSION = 1
+const HOST = process.env.HOSTNAME    || 'unknown';
 const ENV  = process.env.ENVIRONMENT || 'unknown';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World! v${VERSION} in ${ENV}`);
+  res.send(`Hello World! v${VERSION} in ${ENV} on $(HOST)\n`);
 });
 
 app.listen(PORT, HOST => 
