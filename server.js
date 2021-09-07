c52a12be189004f46bc3bdc481f3393f5a5edbe2
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-07 14:03:15 -0500

    Add env visibility

--- server.js
+++ server.js
@@ -3,16 +3,17 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 const VERSION = 6
+const ENV  = process.env.ENVIRONMENT || 'unknown';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World! v${VERSION}`);
+  res.send(`Hello World! v${VERSION} in ${ENV}`);
 });
 
 app.listen(PORT, HOST => 
-    console.log(`Running v${VERSION} on http://${HOST}:${PORT}`)
+    console.log(`Running v${VERSION} in ${ENV} on http://${HOST}:${PORT}`)
 );
