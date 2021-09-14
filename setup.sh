a102cefbc1359f8e919c7b2c3df9afc3cea20814
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-14 10:28:22 -0500

    Adding dev route

--- setup.sh
+++ setup.sh
@@ -21,13 +21,19 @@ oc-login 1
 cd haproxy
 oc delete --ignore-not-found=1 -f namespace.yaml
 oc apply -f namespace.yaml
-HAPROXY_LB_ROUTE=multi-cloud.kcdc.rhlabs.io
+# Define the variable of `HELLO_INGRESS`
+HELLO_INGRESS_BASE=kcdc.rhlabs.io
+HELLO_INGRESS_SUBDOMAIN=multi-cloud
+HAPROXY_LB_ROUTE=$HELLO_INGRESS_SUBDOMAIN.$HELLO_INGRESS_BASE
 oc -n multi-cloud-lb create route edge multi-cloud-lb \
    --service=multi-cloud-lb-service --port=8080 --insecure-policy=Allow \
    --hostname=${HAPROXY_LB_ROUTE}
 echo $HAPROXY_LB_ROUTE
-# Define the variable of `HELLO_INGRESS`
-HELLO_INGRESS=multi-cloud.kcdc.rhlabs.io
+HAPROXY_DEV_LB_ROUTE=$HELLO_INGRESS_SUBDOMAIN.dev.$HELLO_INGRESS_BASE
+oc -n multi-cloud-lb create route edge multi-cloud-dev-lb \
+   --service=multi-cloud-lb-service --port=8080 --insecure-policy=Allow \
+   --hostname=${HAPROXY_DEV_LB_ROUTE}
+echo $HAPROXY_DEV_LB_ROUTE
 # Define the variable of `HELLO_CLUSTER1`
 oc-login 1
 HELLO_CLUSTER1=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
@@ -40,9 +46,8 @@ HELLO_CLUSTER3=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster 
 # Copy the sample configmap
 rm -f haproxy; cp haproxy.tmpl haproxy
 # Update the HAProxy configuration
-sed -i "/option httpchk GET/a \ \ \ \ http-request set-header Host ${HELLO_INGRESS}" haproxy
-# Replace the value with the variable `HELLO_INGRESS`
-sed -i "s/<hello_lb_hostname>/${HELLO_INGRESS}/g" haproxy
+sed -i "/INGRESS_BASE/${HELLO_INGRESS_BASE}/g" haproxy
+sed -i "/INGRESS_SUBDOMAIN/${HELLO_INGRESS_SUBDOMAIN}/g" haproxy
 # Replace the value with the variable `HELLO_CLUSTER1`
 sed -i "s/<server1_name> <server1_hello_route>:<route_port>/cluster1 ${HELLO_CLUSTER1}:80/g" haproxy
 # Replace the value with the variable `HELLO_CLUSTER2`
@@ -57,7 +62,8 @@ oc -n multi-cloud-lb create -f haproxy-clusterip-service.yaml
 oc -n multi-cloud-lb create -f haproxy-deployment.yaml
 oc -n multi-cloud-lb get pods
 HELLO_LB_ROUTE=$(oc -n multi-cloud-lb get route multi-cloud-lb -o jsonpath='{.status.ingress[*].host}')
-curl -k https://${HELLO_LB_ROUTE}
+curl -k https://${HAPROXY_LB_ROUTE}
+curl -k https://${HAPROXY_DEV_LB_ROUTE}
 cd -
 
 # create application
