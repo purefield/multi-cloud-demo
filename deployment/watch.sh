b59ea10155db0a5277cd0e85c5ccb6e26def848e
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-14 11:22:33 -0500

    Fix host conflict

--- deployment/watch.sh
+++ deployment/watch.sh
@@ -1,1 +1,2 @@
-watch curl -s $(. /srv/login.sh && for i in 1 2 3; do echo -n "http://hello-multi-cloud.apps.${clusters[$i]} "; done)
+watch curl -s https://multi-cloud.kcdc.rhlabs.io https://multi-cloud.dev.kcdc.rhlabs.io  \
+  $(. /srv/login.sh && for i in 1 2 3; do echo -n "http://hello-multi-cloud.apps.${clusters[$i]} "; done)
