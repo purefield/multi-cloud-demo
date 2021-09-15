f2ac6fdad89c817b51635bb6005dcb7be4e164d9
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-15 13:50:08 -0500

    Tweak demo

--- demo.sh
+++ demo.sh
@@ -6,8 +6,8 @@ cluster2=$(echo ${clusters[2]} | cut -d\. -f1)
 cluster3=$(echo ${clusters[3]} | cut -d\. -f1)
 
 
-#___ "Remove app-env-hello labels"
-#oc label ManagedCluster --all app-env-hello-
+___ "Remove app-env-hello labels"
+oc label ManagedCluster --all app-env-hello-
 
 ___ "Add app-env-hello labels to all clusters"
 oc label ManagedCluster --overwrite=true -l name=$cluster1 app-env-hello=development
@@ -19,3 +19,6 @@ oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=product
 
 ___ "Move cluster 3 to development"
 oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=development
+
+___ "Remove app from cluster 3"
+oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello-
