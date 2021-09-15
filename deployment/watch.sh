b01a5ddb630b1d7905eb64eff139009cbae586a9
Daniel Schimpfoessl <dschimpf@redhat.com>
2021-09-15 14:25:55 -0500

    Tweak demo

--- deployment/watch.sh
+++ deployment/watch.sh
@@ -1,1 +1,2 @@
-watch "echo '* Global Load Balancer'; echo ' - Production'; curl -ks https://multi-cloud.kcdc.rhlabs.io; echo ' - Development'; curl -sk https://multi-cloud.dev.kcdc.rhlabs.io; echo; echo; echo '* Individual Clusters'; curl -sk $(. /srv/login.sh && for i in 1 2 3; do echo -n "http://hello-multi-cloud.apps.${clusters[$i]} "; done)"
+watch "function curl { $(which curl) --max-time 0.5 -ks \$@ | grep -i hello; };"\
+"echo '* Global Load Balancer'; echo ' - Production'; curl https://multi-cloud.kcdc.rhlabs.io; echo ' - Development'; curl https://multi-cloud.dev.kcdc.rhlabs.io; echo; echo; echo '* Individual Clusters'; curl $(. /srv/login.sh && for i in 1 2 3; do echo -n "http://hello-multi-cloud.apps.${clusters[$i]} "; done)"
