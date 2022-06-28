fc457c21cb797b4386c552a954a40c5ee7c3e393
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-06-28 10:58:50 -0500

    Reset version, add infra

--- server.js
+++ server.js
@@ -3,19 +3,20 @@
 const express = require('express');
 
 // Constants
-const VERSION  = 2
+const VERSION  = 1
 const HOST     = '0.0.0.0';
 const PORT     = process.env.PORT || 8080;
 const HOSTNAME = process.env.HOSTNAME    || 'unknown';
 const ENV      = process.env.ENVIRONMENT || 'unknown';
+const INFRA    = process.env.INFRA       || 'unknown';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World! v${VERSION} in ${ENV} on ${HOSTNAME}\n`);
+  res.send(`Hello World! v${VERSION} in ${ENV}-${INFRA} on ${HOSTNAME}\n`);
   console.log("Request at: " + new Date().toString());
 });
 
 app.listen(PORT, HOST => 
-    console.log(`Running v${VERSION} in ${ENV} on http://${HOST}:${PORT}`)
+    console.log(`Running v${VERSION} in ${ENV}-${INFRA} on http://${HOST}:${PORT}`)
 );
