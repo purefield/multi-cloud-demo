3ced72ace8c2b1967d6f89dcf6a5fa6e9418fe69
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-06-29 18:07:39 -0500

    Adding platform details to server output

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ const HOST        = '0.0.0.0';
 const PORT        = process.env.PORT || 8080;
 const HOSTNAME    = process.env.HOSTNAME    || 'unknown';
 const ENV         = process.env.ENVIRONMENT || 'unknown';
+const CLUSTER     = process.env.CLUSTER     || 'unknown';
 const PLATFORM    = process.env.PLATFORM    || 'unknown';
 const OCP_VERSION = process.env.VERSION     || 'unknown';
 
 // App
 const app = express();
+const ver = `v${VERSION} in ${ENV} on ${HOSTNAME}.${CLUSTER} OCP v${OCP_VERSION} in ${PLATFORM}`;
 app.get('/', (req, res) => {
-  res.send(`Hello World! v${VERSION} in ${ENV}-${PLATFORM}:${OCP_VERSION} on ${HOSTNAME}\n`);
+  res.send(`Hello World! ${ver}\n`);
   console.log("Request at: " + new Date().toString());
 });
 
 app.listen(PORT, HOST => 
-    console.log(`Running v${VERSION} in ${ENV}-${PLATFORM}:${OCP_VERSION} on http://${HOST}:${PORT}`)
+    console.log(`Running ${ver} on http://${HOST}:${PORT}`)
 );
