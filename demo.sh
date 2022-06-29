5bf02e2ee72909d3eba6fe416b75aace8e5533e0
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-06-29 15:57:23 -0500

    Add 4th cluster to pools.

--- demo.sh
+++ demo.sh
@@ -4,6 +4,13 @@ oc-login acm
 cluster1=$(echo ${clusters[2]} | cut -d\. -f1)
 cluster2=$(echo ${clusters[3]} | cut -d\. -f1)
 cluster3=$(echo ${clusters[4]} | cut -d\. -f1)
+cluster4=$(echo ${clusters[5]} | cut -d\. -f1)
+
+___ "Remove App Subscription Report (tmp)"
+oc delete appsubreport -n ocp ocp
+oc delete appsubreport -n aws aws
+oc delete appsubreport -n azure azure
+oc delete appsubreport -n gcp gcp
 
 ___ "Remove app-env-hello labels"
 oc label ManagedCluster --all app-env-hello-
@@ -12,6 +19,7 @@ ___ "Add app-env-hello labels to all clusters"
 oc label ManagedCluster --overwrite=true -l name=$cluster1 app-env-hello=development
 oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=development
 oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=production
+oc label ManagedCluster --overwrite=true -l name=$cluster4 app-env-hello=production
 
 ___ "Move cluster 2 to production"
 oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=production
