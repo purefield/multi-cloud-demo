2da5cb8f053b6c96c96550b37bc2abba994db8b0
Daniel Schimpfoessl <dschimpf@redhat.com>
2022-07-05 16:04:20 -0500

    Fix gitlab secret distribution using ACM Governance.

--- configure-acm.sh
+++ configure-acm.sh
@@ -37,18 +37,17 @@ oc-login acm
 
 # names
 cluster1="local-cluster"
-oc label ManagedCluster -l name=$cluster1 usage=gitlab --overwrite=true
 for i in 2 3 4 5; do
   cluster=$(echo ${clusters[$i]} | cut -d\. -f1)
   oc label ManagedCluster -l name=$cluster usage=gitlab --overwrite=true
 done
-for i in 2 3 4 5; do
-  oc-login $i
-  oc create namespace gitlab-auth
-  oc project gitlab-auth
-  oc apply -f secrets.yaml
-  oc secrets link default gitlab-access-token --for=pull
-done
+#for i in 2 3 4 5; do
+#  oc-login $i
+#  oc create namespace gitlab-auth
+#  oc project gitlab-auth
+#  oc apply -f secrets.yaml
+#  oc secrets link default gitlab-access-token --for=pull
+#done
 
 ./setup.sh
 echo run ./demo.sh
