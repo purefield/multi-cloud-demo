942b7508393f5f705f4b9bfc469e9c5521bedd72
dschimpf@redhat.com <dds@redhat.com>
2023-02-06 13:56:58 -0500

    Adding run from quay.io line

--- tomcat-demo/demo/run.sh
+++ tomcat-demo/demo/run.sh
@@ -1,2 +1,3 @@
 podman build -f MultistageContainerfile -t hello-java
 podman run --env-file environment.txt --rm -d -p 8080:8080 hello-java:latest
+echo "podman run --env-file environment.txt --rm -d -p 8080:8080 quay.io/dds/multi-cloud/hello-java"
