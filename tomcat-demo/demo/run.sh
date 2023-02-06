d7dbb56918cab53a7e999d7533ece97e0bb78e21
dschimpf@redhat.com <dds@redhat.com>
2023-02-06 15:37:57 -0500

    Fix format

--- tomcat-demo/demo/run.sh
+++ tomcat-demo/demo/run.sh
@@ -1,4 +1,4 @@
 podman build -f MultistageContainerfile -t hello-java
 podman stop hello-java
 podman run --env-file environment.txt --rm -d -p 8080:8080 --name hello-java hello-java:latest 
-echo 'mount /dev/sda/ /mnt; podman run --rm -d -p 8080:8080 --name hello-java -e PLATFORM="$(cat /mnt/platform)" -e CLUSTER="$(cat /mnt/cluster)" -e CLUSTER="$(cat /mnt/product)" -e VERSION="$(cat /mnt/version)" -e ENVIRONMENT="$(cat /mnt/environment)" -e HOSTNAME="$(hostname -s) -e TYPE="tomcat-vm"" quay.io/dds/multi-cloud/hello-java'
+echo 'mount /dev/sda/ /mnt; podman run --rm -d -p 8080:8080 --name hello-java -e PLATFORM="$(cat /mnt/platform)" -e CLUSTER="$(cat /mnt/cluster)" -e CLUSTER="$(cat /mnt/product)" -e VERSION="$(cat /mnt/version)" -e ENVIRONMENT="$(cat /mnt/environment)" -e HOSTNAME="$(hostname -s)" -e TYPE="tomcat-vm" quay.io/dds/multi-cloud/hello-java'
