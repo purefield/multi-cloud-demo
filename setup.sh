1476d6d03f25bbe0b123324c266b1214622ebf22
dschimpf@redhat.com <dds@redhat.com>
2023-02-14 12:40:23 -0500

    Simplify haproxy config creation to also allow for dynamic number of backends.

--- setup.sh
+++ setup.sh
@@ -55,34 +55,35 @@ oc -n multi-cloud-lb create route edge multi-cloud-dev-lb \
    --service=multi-cloud-lb-service --port=8080 --insecure-policy=Allow \
    --hostname=${HAPROXY_DEV_LB_ROUTE}
 echo $HAPROXY_DEV_LB_ROUTE
-# Define the variable of `HELLO_CLUSTER1`
-oc-login ocp1
-HELLO_CLUSTER1=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
-# Define the variable of `HELLO_CLUSTER2`
-oc-login ocp2
-HELLO_CLUSTER2=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
-# Define the variable of `HELLO_CLUSTER3`
-oc-login ocp3
-HELLO_CLUSTER3=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
-# Define the variable of `HELLO_CLUSTER4`
-oc-login ocp4
-HELLO_CLUSTER4=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
-# Copy the sample configmap
-rm -f haproxy; cp haproxy.tmpl haproxy
-# Update the HAProxy configuration
-sed -i "s/INGRESS_BASE/${HELLO_INGRESS_BASE}/g" haproxy
-sed -i "s/INGRESS_SUBDOMAIN/${HELLO_INGRESS_SUBDOMAIN}/g" haproxy
-# Replace the value with the variable `HELLO_CLUSTER1`
-sed -i "s/<server1_name> <server1_hello_route>:<route_port>/cluster1 ${HELLO_CLUSTER1}:80/g" haproxy
-# Replace the value with the variable `HELLO_CLUSTER2`
-sed -i "s/<server2_name> <server2_hello_route>:<route_port>/cluster2 ${HELLO_CLUSTER2}:80/g" haproxy
-# Replace the value with the variable `HELLO_CLUSTER3`
-sed -i "s/<server3_name> <server3_hello_route>:<route_port>/cluster3 ${HELLO_CLUSTER3}:80/g" haproxy
-# Replace the value with the variable `HELLO_CLUSTER4`
-sed -i "s/<server4_name> <server4_hello_route>:<route_port>/cluster4 ${HELLO_CLUSTER4}:80/g" haproxy
-# Create the configmap
-oc-login acm
+# - replaced with update-haproxy.sh - # Define the variable of `HELLO_CLUSTER1`
+# - replaced with update-haproxy.sh - oc-login ocp1
+# - replaced with update-haproxy.sh - HELLO_CLUSTER1=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
+# - replaced with update-haproxy.sh - # Define the variable of `HELLO_CLUSTER2`
+# - replaced with update-haproxy.sh - oc-login ocp2
+# - replaced with update-haproxy.sh - HELLO_CLUSTER2=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
+# - replaced with update-haproxy.sh - # Define the variable of `HELLO_CLUSTER3`
+# - replaced with update-haproxy.sh - oc-login ocp3
+# - replaced with update-haproxy.sh - HELLO_CLUSTER3=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
+# - replaced with update-haproxy.sh - # Define the variable of `HELLO_CLUSTER4`
+# - replaced with update-haproxy.sh - oc-login ocp4
+# - replaced with update-haproxy.sh - HELLO_CLUSTER4=hello-multi-cloud.$(oc get ingresses.config.openshift.io cluster -o jsonpath='{ .spec.domain }')
+# - replaced with update-haproxy.sh - # Copy the sample configmap
+# - replaced with update-haproxy.sh - rm -f haproxy; cp haproxy.tmpl haproxy
+# - replaced with update-haproxy.sh - # Update the HAProxy configuration
+# - replaced with update-haproxy.sh - sed -i "s/INGRESS_BASE/${HELLO_INGRESS_BASE}/g" haproxy
+# - replaced with update-haproxy.sh - sed -i "s/INGRESS_SUBDOMAIN/${HELLO_INGRESS_SUBDOMAIN}/g" haproxy
+# - replaced with update-haproxy.sh - # Replace the value with the variable `HELLO_CLUSTER1`
+# - replaced with update-haproxy.sh - sed -i "s/<server1_name> <server1_hello_route>:<route_port>/cluster1 ${HELLO_CLUSTER1}:80/g" haproxy
+# - replaced with update-haproxy.sh - # Replace the value with the variable `HELLO_CLUSTER2`
+# - replaced with update-haproxy.sh - sed -i "s/<server2_name> <server2_hello_route>:<route_port>/cluster2 ${HELLO_CLUSTER2}:80/g" haproxy
+# - replaced with update-haproxy.sh - # Replace the value with the variable `HELLO_CLUSTER3`
+# - replaced with update-haproxy.sh - sed -i "s/<server3_name> <server3_hello_route>:<route_port>/cluster3 ${HELLO_CLUSTER3}:80/g" haproxy
+# - replaced with update-haproxy.sh - # Replace the value with the variable `HELLO_CLUSTER4`
+# - replaced with update-haproxy.sh - sed -i "s/<server4_name> <server4_hello_route>:<route_port>/cluster4 ${HELLO_CLUSTER4}:80/g" haproxy
+# - replaced with update-haproxy.sh - # Create the configmap
+# - replaced with update-haproxy.sh - oc-login acm
 oc -n multi-cloud-lb create configmap haproxy --from-file=haproxy
+./update-haproxy.sh
 # create haproxy and check it
 oc -n multi-cloud-lb create -f haproxy-clusterip-service.yaml
 oc -n multi-cloud-lb create -f haproxy-deployment.yaml
