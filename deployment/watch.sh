b9664f98184064f96496c9e14d11889738f55d04
Daniel Schimpfoessl <dds@redhat.com>
2024-03-17 09:58:56 -0500

    fix odd syntax error of unexpected closing curly brace.

--- deployment/watch.sh
+++ deployment/watch.sh
@@ -1,13 +1,4 @@
+#!/bin/bash
 oc config use-context acm
-watch "function curl { $(which curl) --max-time 0.5 -ks \$@ | grep -i hello; };"\
-"echo '* Global Load Balancer'; 
 
-echo ' - Production https://multi-cloud.box--eng.com'; 
-curl https://multi-cloud.box--eng.com; echo ' '; 
-
-echo ' - Development https://multi-cloud.dev.box--eng.com'; 
-curl https://multi-cloud.dev.box--eng.com; 
-
-echo ' '; echo ' '; echo '* Individual Clusters'; 
-curl $(for cluster in $(oc get ManagedClusters -o yaml | grep 'console-' | perl -pe 's/[^v]+value[^.]+//'); do echo -n "http://hello-dev-multi-cloud$cluster http://hello-prod-multi-cloud$cluster "; done)
-"
+watch bash -c "echo '* Global Load Balancer'; echo ' - Production https://multi-cloud.box--eng.com'; ./curl_filtered https://multi-cloud.box--eng.com; echo ' '; echo ' - Development https://multi-cloud.dev.box--eng.com'; ./curl_filtered https://multi-cloud.dev.box--eng.com; echo ' '; echo ' '; echo '* Individual Clusters'; ./curl_filtered $(for cluster in $(oc get ManagedClusters -o yaml | grep 'console-' | perl -pe 's/[^v]+value[^.]+//'); do echo -n "http://hello-dev-multi-cloud$cluster http://hello-prod-multi-cloud$cluster "; done) "
