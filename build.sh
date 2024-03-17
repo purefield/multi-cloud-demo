ccd107882aa86e44eb6bdaf459b2cfca0ea50d27
Daniel Schimpfoessl <dds@redhat.com>
2024-03-17 10:21:10 -0500

    Different version for dev and prod

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ podman build . -t hello
 podman login registry.gitlab.com -u openshift-token -p "$token"
 # version 1 as v1
 perl -pe 's/(VERSION\D+)\d+$/${1}1/' -i server.js
-podman tag hello:latest registry.gitlab.com/gitlab-com/partners/alliance/ibm-red-hat/sandbox/customer-k/multi-cloud-demo/hello:v1
-podman push registry.gitlab.com/gitlab-com/partners/alliance/ibm-red-hat/sandbox/customer-k/multi-cloud-demo/hello:v1
+podman tag hello:latest registry.gitlab.com/gitlab-com/partners/alliance/ibm-red-hat/sandbox/customer-k/multi-cloud-demo/hello:prod
+podman push registry.gitlab.com/gitlab-com/partners/alliance/ibm-red-hat/sandbox/customer-k/multi-cloud-demo/hello:prod
 # version 2 as latest
 perl -pe 's/(VERSION\D+)\d+$/${1}2/' -i server.js
 podman tag hello:latest registry.gitlab.com/gitlab-com/partners/alliance/ibm-red-hat/sandbox/customer-k/multi-cloud-demo/hello:latest
