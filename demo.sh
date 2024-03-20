317fdc072b52cb26cf8af0f412f8fc7217eb92d6
Daniel Schimpfoessl <dds@redhat.com>
2024-03-20 09:50:02 -0500

    Craft a customer focused story

--- demo.sh
+++ demo.sh
@@ -10,24 +10,35 @@ cluster5=${clusters[4]}
 ___ "Remove app-env-hello labels"
 oc label ManagedCluster --all app-env-hello-
 
-___ "Add app-env-hello labels to all clusters"
+___ "Add app-env-hello labels to on-prem clusters"
 oc label ManagedCluster --overwrite=true -l name=$cluster1 app-env-hello=development
-oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=development
-oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=production
-oc label ManagedCluster --overwrite=true -l name=$cluster4 app-env-hello=production
 oc label ManagedCluster --overwrite=true -l name=$cluster5 app-env-hello=development
+
+___ "Add app-env-hello label for development to $cluster2"
+oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=development
+
+___ "Add app-env-hello label for development to $cluster3"
+oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=development
+
+___ "Add app-env-hello label for development to $cluster4"
+oc label ManagedCluster --overwrite=true -l name=$cluster4 app-env-hello=development
 
 ___ "Move cluster $cluster2 to production"
 oc label ManagedCluster --overwrite=true -l name=$cluster2 app-env-hello=production
 
-___ "Move cluster $cluster3 to development"
-oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=development
+___ "Move cluster $cluster3 to production"
+oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello=production
 
-___ "Move cluster $cluster1 to production"
-oc label ManagedCluster --overwrite=true -l name=$cluster1 app-env-hello=production
+___ "Move cluster $cluster4 to production"
+oc label ManagedCluster --overwrite=true -l name=$cluster4 app-env-hello=production
 
 ___ "Remove app from cluster $cluster1"
 oc label ManagedCluster --overwrite=true -l name=$cluster1 app-env-hello-
+
+___ "Remove app from cluster $cluster3"
+oc label ManagedCluster --overwrite=true -l name=$cluster3 app-env-hello-
+
+exit 0
 
 ___ "Create code change"
 git checkout development
